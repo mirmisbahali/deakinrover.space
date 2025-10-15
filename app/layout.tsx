be1365ee3c42317edea120f7dcf38ac7df1246fd
Mir Misbah Ali <mirmisbahali@hotmail.com>
2025-10-15 20:18:42 +1100

    added Google Tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -92,6 +92,16 @@ export default function RootLayout({ children }: PropsWithChildren) {
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
         />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NQJPHK8J');`
+          }}
+        />
       </head>
       <body
         className={cn(
@@ -101,6 +111,15 @@ export default function RootLayout({ children }: PropsWithChildren) {
           montserrat.variable
         )}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NQJPHK8J"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <StarsCanvas />
         <Navbar />
         {children}
